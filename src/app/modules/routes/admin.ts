a63796b28af25c9ea7c0dffe4b2fd53b5dd52049
ashiq72 <ahmedashik18k@gmail.com>
2026-02-17 11:37:32 +0600

    Updated

--- src/app/modules/routes/admin.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { AdminController } from "../controllers/admin";
-
-const router = express.Router();
-
-router.get("/", AdminController.getAllFromDB);
-
-export const AdminRoutes = router;
--- src/app/modules/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { userController } from "../controllers/user";
-
-const router = express.Router();
-
-router.post("/", userController.createAdmin);
-
-export const userRoute = router;
